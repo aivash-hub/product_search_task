8602ad08070ff9e68c4cfac4d5f5fc3d4d60159f
Антон Ивашкин <aivash.tech@gmail.com>
2026-01-19 03:42:05 +0300

    Add Product and Category models and migrations

---
+++ database/migrations/2026_01_18_214802_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->fullText('name');
+
+            $table->decimal('price', 10, 2);
+            $table->decimal('rating', 3, 2)->default(0.00);
+
+            $table->boolean('in_stock')->default(false);
+
+            $table->foreignId('category_id')
+                ->nullable()
+                ->constrained('categories')
+                ->nullOnDelete();
+
+            $table->timestamps();
+
+            $table->index('price');
+            $table->index('rating');
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
