20c097fe5b72a3ee127366e60026ce8b33b3fe4d
Антон Ивашкин <aivash.tech@gmail.com>
2026-01-19 14:03:32 +0300

    Add products API route and controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Product::query();
+
+        // Для фильтров
+
+        return $query->paginate(10);
+    }
+}
