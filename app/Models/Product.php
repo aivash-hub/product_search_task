1179d2f72cfed079b2296911e2ef1d2811ca7ca5
Антон Ивашкин <aivash.tech@gmail.com>
2026-01-19 21:32:08 +0300

    Add model relationships and factories

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,25 @@ namespace App\Models;
 
 use App\Filters\ProductFilter;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function scopeFilter(Builder $query, array $filters): Builder
     {
         $filter = new ProductFilter();
 
-        $query = $filter->applyFilter($query, $filters);
-
-        return $filter->applySorting($query, $filters['sort'] ?? null);
+        return $filter->applySorting(
+            $filter->applyFilter($query, $filters),
+            $filters['sort'] ?? null
+        );
     }
 }
